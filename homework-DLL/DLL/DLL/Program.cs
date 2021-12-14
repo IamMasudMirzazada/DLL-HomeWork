b420f93ce0ce26d9de7ba227a426a5348564c5de
IamMasudMirzazada <masudjm@code.edu.az>
2021-12-13 23:28:12 -0800

    task

---
+++ homework-DLL/DLL/DLL/Program.cs
@@ -0,0 +1,17 @@
+﻿using DLLLibrary1;
+using System;
+
+namespace DLL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator cal = new Calculator();
+            Console.WriteLine(cal.Ferq(10,7));
+            Console.WriteLine(cal.Sum(2,3));
+            Console.WriteLine(cal.Vurma(4,5));
+            Console.WriteLine(cal.Qismet(30,6));
+        }
+    }
+}
---
+++ homework-DLL/DLLLibrary1/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DLLLibrary1
+{
+    public class Calculator
+    {
+
+
+        public int Sum(int a,int b)
+        {
+            return a + b;
+        }
+        public int Ferq (int a, int b)
+        {
+            return a - b;
+        }
+        public int Vurma(int a, int b)
+        {
+            return a * b;
+        }
+        public int Qismet(int a, int b)
+        {
+            return a/b;
+        }
+
+    }
+}
